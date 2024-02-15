4ff22afd4af6311f134310199de3f1055cfd538a
David69699 <abutidzedato18@gmail.com>
2024-02-15 21:47:35 +0400

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,6 @@ export default function Header() {
     <div>
         <header>
             <h1>Where in the world?</h1>
-
-            <div className='flex'>
-                <img src="./dark.svg" alt="" />
-                <h4>Dark Mode</h4>
-            </div>
         </header>
     </div>
   )
